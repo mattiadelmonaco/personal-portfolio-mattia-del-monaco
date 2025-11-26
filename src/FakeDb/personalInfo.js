692b9f5af7db89007db87a7d07a8e1366e82f933
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-11-26 20:28:25 +0100

    add personalInfo.js

---
+++ src/FakeDb/personalInfo.js
@@ -0,0 +1,33 @@
+export const personalInfo = {
+  // info base - basic info
+  name: "Mattia",
+  surname: "Del Monaco",
+  role: "Jr Full Stack Web Developer",
+  //   avatar
+  avatar: "/images/avatar-mattia-del-monaco.png",
+  //   contatti - contacts
+  location: { it: "Sulmona (AQ), Italia", en: "Sulmona (AQ), Italy" },
+  phoneNumber: "+39 347 325 7478",
+  email: "mattia.delmonaco@gmail.com",
+  //   social
+  social: {
+    linkedin: "https://www.linkedin.com/in/mattia-del-monaco-b69b2023a",
+    github: "https://github.com/mattiadelmonaco",
+  },
+  //   lingue parlate - spoken languages
+  languages: [
+    {
+      name: { it: "Italiano", en: "Italian" },
+      level: { it: "Madrelingua", en: "Native" },
+    },
+    {
+      name: { it: "Inglese", en: "English" },
+      level: { it: "B1", en: "B1" },
+    },
+  ],
+  // disponibilità - availability
+  availability: {
+    it: "Disponibile per opportunità",
+    en: "Open to opportunities",
+  },
+};
